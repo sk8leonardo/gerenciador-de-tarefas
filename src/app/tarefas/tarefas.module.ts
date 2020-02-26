a1bab292f1f1e3bd6c78198ce32f5de150988307
Leonardo Minhoto <sk8_leonardo@yahoo.com.br>
2020-02-26 15:19:02 -0300

    dia2

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,15 @@
+import { TarefaService } from './shared/tarefa.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
