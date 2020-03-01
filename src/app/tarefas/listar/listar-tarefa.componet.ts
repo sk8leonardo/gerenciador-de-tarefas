a6d269d13216b91e27d97d92ac1e945742d4dcde
Leonardo Minhoto <sk8_leonardo@yahoo.com.br>
2020-03-01 17:47:52 -0300

    Aula4

--- src/app/tarefas/listar/listar-tarefa.componet.ts
+++ src/app/tarefas/listar/listar-tarefa.componet.ts
@@ -7,10 +7,11 @@ import { TarefaService, Tarefa } from '../shared';
   styleUrls: ['./listar-tarefa.component.css']
 })
 export class ListarTarefaComponent implements OnInit {
-    tarefas: Tarefa[];
-    tarefaService: any;
+  tarefas: Tarefa[];
 
-  constructor() { }
+  constructor(
+    private tarefaService: TarefaService
+  ) { }
 
   ngOnInit() {
       this.tarefas = this.listarTodos();
@@ -20,4 +21,16 @@ export class ListarTarefaComponent implements OnInit {
       return this.tarefaService.listarTodos();
   }
 
+  remover($event:any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?'))
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if(confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?'))
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+  }
 }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,17 +1,22 @@
-import { TarefaService } from './shared';
+import { TarefaService, Tarefa, TarefaConcluidaDirective } from './shared';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarTarefaComponent } from './listar';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
 
 @NgModule({
   declarations: [
-    ListarTarefaComponent],
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [
     TarefaService
